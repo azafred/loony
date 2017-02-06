de54aff5b1e96dc0336d827243664ec73f9c1fea
Fred Vassard <fredvassard@sparkcentral.com>
2017-02-06 09:49:30 -0800

    initial project defs

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('LICENSE') as f:
     license = f.read()
 
 setup(
-    name='sample',
+    name='loony',
     version='0.1.0',
     description='Sample package for Python-Guide.org',
     long_description=readme,
@@ -22,7 +22,7 @@ setup(
     test_suite="nose.collector",
     entry_points={
         'console_scripts': [
-          'sample = sample.main:main',
+          'loony = loony.main:main',
         ],
       },
       classifiers=[
