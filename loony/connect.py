df756a6a24f9694f34c8176232154be19f2f1ae7
Fred Vassard <fredvassard@sparkcentral.com>
2017-02-07 15:20:51 -0800

    WIP

--- loony/connect.py
+++ loony/connect.py
@@ -1,19 +1,28 @@
 from subprocess import call
 from display import display_results_ordered
+from settings import *
 import sys
 
 
 def connect_to(instances):
-    dest = raw_input("Connect to instance number: (0 to quit) ")
-    dest = int(dest.strip())
-    print "dest: %s" % dest
-    if dest == 0:
+
+    if len(instances) == 1:
+        print("Note: make sure you are connected to the VPN!")
+        ip = instances[0]['priv_ip']
+        print("Connecting to %s" % ip)
+        call("ssh -l " + ssh_as + " " + ip, shell=True)
         sys.exit(0)
-    for inst in instances:
-        if int(inst['index']) == dest:
-            ip = inst['priv_ip']
-            print("Note: make sure you are connected to the VPN!")
-            print("connecting to: %s " % ip)
-            call("ssh -l ubuntu " + ip, shell=True)
+    else:
+        dest = raw_input("Connect to instance number: (0 to quit) ")
+        dest = int(dest.strip())
+        print "dest: %s" % dest
+        if dest == 0:
             sys.exit(0)
+        for inst in instances:
+            if int(inst['index']) == dest:
+                ip = inst['priv_ip']
+                print("Note: make sure you are connected to the VPN!")
+                print("connecting to: %s " % ip)
+                call("ssh -l " + ssh_as + " " + ip, shell=True)
+                sys.exit(0)
     print("An error has occured.")
--- loony/settings.py
+++ loony/settings.py
@@ -1,3 +1,4 @@
 cache_file = '/tmp/aws_inventory'
 default_aws_domains = ['ops', 'qa', 'prod', 'experiments']
 prefered_output = "all"
+ssh_as = 'ubuntu'
