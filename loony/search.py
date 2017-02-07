04c68a1ce37c6c8539084183c52541a384ef7fbb
Fred Vassard <fredvassard@sparkcentral.com>
2017-02-07 15:49:37 -0800

    WIP

--- loony/search.py
+++ loony/search.py
@@ -6,9 +6,9 @@ def searchfor(items, oroperand=False):
     if __builtin__.verbose:
         print 'verbose'
     aws = aws_inventory()
-    result = []
+    or_result = []
     result_per_item = {}
-    out = []
+    and_result = []
     counter = {}
     index = 1
     for item in items:
@@ -20,24 +20,28 @@ def searchfor(items, oroperand=False):
                     counter[inst['id']] += 1
                 except KeyError:
                     counter[inst['id']] = 1
-                if inst not in result:
-                    inst['index'] = index
-                    index += 1
-                    result.append(inst)
+                if inst not in or_result:
+                    or_result.append(inst)
                 result_per_item[item].append(inst)
     if not oroperand:
         for k,v in counter.iteritems():
             if v == len(items):
                 for inst in aws:
                     if inst['id'] == k:
-                        out.append(inst)
-        if len(out) >= 1:
-            display_results_ordered(out)
-        return out
+                        and_result.append(inst)
+        for r in and_result:
+            r['index'] = index
+            index += 1
+        if len(and_result) >= 1:
+            display_results_ordered(and_result)
+        return and_result
     else:
-        if len(result) >= 1:
-            display_results_ordered(result)
-        return result
+        for r in or_result:
+            r['index'] = index
+            index += 1
+        if len(or_result) >= 1:
+            display_results_ordered(or_result)
+        return or_result
 
 def pub_ip():
     aws = aws_inventory()
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('LICENSE') as f:
 
 setup(
     name='loony',
-    version='0.1.8',
+    version='0.1.9',
     description='Sample package for Python-Guide.org',
     long_description=readme,
     author='Fred Vassard',
