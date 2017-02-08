9d863abdf6e5144d18855f7a3e546b969647417e
Fred Vassard <fredvassard@sparkcentral.com>
2017-02-08 12:03:48 -0800

    Trying to setup version based on git tags

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('LICENSE') as f:
 # Fetching version from git tag...
 version_py = os.path.join(os.path.dirname(__file__), 'version.py')
 try:
-    version_git = subprocess.check_output(["git", "describe"]).rstrip()
+    version_git = subprocess.check_output(["git", "describe", "--tags"]).rstrip()
 except:
     with open(version_py, 'r') as fh:
         version_git = open(version_py).read().strip().split('=')[-1].replace('"','')
