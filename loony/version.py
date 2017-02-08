007b2b5033bac2d32ebea22978b5c67c51f77524
Fred Vassard <fredvassard@sparkcentral.com>
2017-02-08 12:10:59 -0800

    last try

--- loony/version.py
+++ loony/version.py
@@ -5,7 +5,8 @@ version_py = os.path.join(os.path.dirname(__file__), 'version.py')
 try:
     version_git = subprocess.check_output(["git", "describe"]).rstrip()
 except:
-    version_git = "0.0.0-error"
+    with open(version_py, 'r') as fh:
+        version_git = open(version_py).read().strip().split('=')[-1].replace('"', '')
 
 
 __version__ = version_git
