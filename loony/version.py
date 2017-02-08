d591d90adac0eefe07d9eea3b310121c70c20c39
Fred Vassard <fredvassard@sparkcentral.com>
2017-02-08 12:13:43 -0800

    ok... tag now working! Huzah!

--- loony/version.py
+++ loony/version.py
@@ -3,7 +3,7 @@ import subprocess
 version_py = os.path.join(os.path.dirname(__file__), 'version.py')
 
 try:
-    version_git = subprocess.check_output(["git", "describe"]).rstrip()
+    version_git = subprocess.check_output(["git", "describe", "--tags"]).rstrip()
 except:
     with open(version_py, 'r') as fh:
         version_git = open(version_py).read().strip().split('=')[-1].replace('"', '')
