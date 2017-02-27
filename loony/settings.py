2ee1b463b7f941c7812334a16651534de7634fed
Fred Vassard <fredvassard@sparkcentral.com>
2017-02-27 09:42:25 -0800

    changing timeout to 24 hours since it is otherwise annoyign and things dont change tht often...

--- loony/settings.py
+++ loony/settings.py
@@ -6,6 +6,6 @@ ignored_aws_domains=['fred']
 default_aws_domains = read_aws_domains(ignored_aws_domains)
 prefered_output = "normal" # could be short, all or a list of columnts
 ssh_as = 'ubuntu'
-cache_lifetime = 60 #in minutes
+cache_lifetime = 1440 #in minutes
 
 
