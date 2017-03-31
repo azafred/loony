f8e6c198a517827330c6e5dcb863f84938481980
Fred Vassard <fred@studyblue.com>
2017-03-31 16:23:00 -0700

    trying some tmux magic...

--- loony/connect.py
+++ loony/connect.py
@@ -2,6 +2,7 @@ from subprocess import call
 from display import display_results_ordered
 from settings import *
 import sys
+import libtmux
 
 
 def connect_to(instances, user='ec2-user'):
@@ -12,6 +13,10 @@ def connect_to(instances, user='ec2-user'):
         print("Connecting to %s" % ip)
         call("ssh -l " + user + " " + ip, shell=True)
         sys.exit(0)
+    elif len(instances) <= 10:
+        # use tmux!
+        init_tmux(instances, user=user)
+        pass
     else:
         dest = raw_input("Connect to instance number: (0 to quit) ")
         dest = int(dest.strip())
@@ -26,3 +31,16 @@ def connect_to(instances, user='ec2-user'):
                 call("ssh -l " + user + " " + ip, shell=True)
                 sys.exit(0)
     print("An error has occured.")
+
+
+def init_tmux(instances, title='loony', cmd='', user='ec2_user'):
+    server = libtmux.Server()
+    session = server.new_session(title)
+    num_of_panes = len(instances)
+    # some logic and if loops here....
+    w = session.new_window(attach=False, window_name="blah")
+    for inst in instances:
+        p1 = w.split_window(attach=False, window_name=inst['Name'])
+        p1.send_keys("ssh -l " + user + " " + inst['ip'])
+    # p2 = w.split_window(attach=False, window_name="blah", vertical=False)
+    session.attach_session()
