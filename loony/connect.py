2c87cced9c0153f59195e9d56270e9b3e8f02e81
Fred Vassard <fred@studyblue.com>
2017-04-03 16:29:22 -0700

    Math added... Tmux now opens the right amount of panes. Also changed ssh options to allow for ephemeral recycled IPs

--- loony/connect.py
+++ loony/connect.py
@@ -6,6 +6,7 @@ import libtmux
 
 
 def connect_to(instances, user=''):
+    ssh_opt = " -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no "
     if user:
         cmd_usr = ' -l %s ' % user
     else:
@@ -15,7 +16,7 @@ def connect_to(instances, user=''):
         print("Note: make sure you are connected to the VPN!")
         ip = instances[0]['priv_ip']
         print("Connecting to %s" % ip)
-        call("ssh" + cmd_usr + ip, shell=True)
+        call("ssh" + ssh_opt + cmd_usr + ip, shell=True)
         sys.exit(0)
     elif len(instances) <= 18:
         # use tmux!
@@ -32,32 +33,53 @@ def connect_to(instances, user=''):
                 ip = inst['priv_ip']
                 print("Note: make sure you are connected to the VPN!")
                 print("connecting to: %s " % ip)
-                call("ssh" + cmd_usr + ip, shell=True)
+                call("ssh" + ssh_opt + cmd_usr + ip, shell=True)
                 sys.exit(0)
     print("An error has occured.")
 
 
 def init_tmux(instances, title='loony', cmd='', user=''):
+    ssh_opt = " -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no "
+    num_panes = 6
     pindex = 0
+    windex = 0
+    new_window = True
     if user:
         cmd_usr = ' -l %s ' % user
     else:
         cmd_usr =' '
     server = libtmux.Server()
     session = server.new_session(title)
-    num_of_panes = len(instances)
     # some logic and if loops here....
-    w = session.new_window(attach=True, window_name=title)
+    w = session.select_window('@0')
     for inst in instances:
-        if pindex % 4 == 0 and pindex != 0:
+        if pindex % num_panes == 0 and pindex != 0:
             blah = title + str(pindex)
-            w = session.new_window(attach=True, window_name=blah)
+            w = session.new_window(attach=False, window_name=blah)
+            windex += 1
+            new_window = True
+            x = '@%s' % windex
+            w = session.select_window(x)
         p = w.split_window(attach=True, vertical=True)
+        if new_window:
+            if windex == 0:
+                index = '%%%i' % (pindex * windex)
+            else:
+                index = '%%%i' % (pindex * windex + windex)
+            print index
+            b = w.select_pane(index)
+            b.cmd('kill-pane')
+            new_window=False
+        # if pindex == 0:
+        #     b = w.select_pane(pindex - 1 )
+        #     b.cmd('kill-pane')
+
         p.send_keys("echo 'connecting to  %s'" % inst['name'])
-        p.send_keys("ssh" + cmd_usr + inst['priv_ip'])
-        p.select_pane()
+        p.send_keys("ssh" + ssh_opt + cmd_usr + inst['priv_ip'])
+        # p.select_pane()
         pindex += 1
         w.select_layout('tiled')
     w.select_layout('tiled')
+    
     # p2 = w.split_window(attach=False, window_name="blah", vertical=False)
     session.attach_session()
