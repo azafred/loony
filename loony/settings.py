77a9862c2b2a934717ffecbdfa7e0865a9675e65
Fred Vassard <fred@studyblue.com>
2017-04-14 15:42:22 -0700

    oopsies

--- loony/settings.py
+++ loony/settings.py
@@ -1,5 +1,3 @@
-from config_parser import read_aws_domains
-
 cache_file = '/tmp/aws_inventory'
 prefered_output = "normal" # could be short, all or a list of columnts
 cache_lifetime = 1440 #in minutes
