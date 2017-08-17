5a291cce51bae72073606b6b07c5d330d34f765e
Fred Vassard <fred@studyblue.com>
2017-08-17 09:10:00 -0700

    removing cocoa requirement

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
     url='https://github.com/azafred/samplemod',
     license=license,
     packages=find_packages(exclude=('tests', 'docs')),
-    install_requires=['libtmux','boto','colorama','prettytable','decorator','pyyaml', 'quik', 'pyobjc-framework-Cocoa'],
+    install_requires=['libtmux','boto','colorama','prettytable','decorator','pyyaml', 'quik'],
     tests_require=['nose', 'testfixtures', 'mock'],
     test_suite="nose.collector",
     entry_points={
