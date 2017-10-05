0aa21c7037d0c4a03ca9d0d5c05a355fc555eed2
Fred Vassard <fred@studyblue.com>
2017-10-05 16:03:14 -0700

    Removing cache expiration when searching for loadtest, since I am not caching anymore...

--- loony/main.py
+++ loony/main.py
@@ -108,8 +108,6 @@ def main(connect=False, running_only=True):
         logging.basicConfig(level=logging.INFO)
     if args.debug:
         logging.basicConfig(level=logging.DEBUG)
-    if 'loadtest' in search or 'staging' in search:
-        expire_cache()
     if nocache:
         expire_cache()
     if listkeys:
