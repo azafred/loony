0678ad6a888487a4de044d1e463a0b768f3afdba
Fred Vassard <fred@studyblue.com>
2017-12-21 16:01:10 -0800

    blah...

--- loony/aws_fetcher.py
+++ loony/aws_fetcher.py
@@ -11,7 +11,7 @@ from operator import itemgetter
 
 @scached(cache_file=cache_file, expiry=timedelta(minutes=cache_lifetime))
 #@animated('Loading data')
-@animation.wait('spinner')
+@animation.wait('Loading data')
 def aws_inventory():
     instances = []
     index = 1
